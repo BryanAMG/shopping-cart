75f4fa1f350836493e3c67dbb112bf865f064279
Bryan Armando <mamanibryan1@gmail.com>
2023-04-06 10:16:56 -0500

    fix import

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import FilterProvider from './context/FiltersProvider'
+// import FilterProvider from './context/FiltersProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
